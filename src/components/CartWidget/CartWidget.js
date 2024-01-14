faec10ceb89c00f9a804ffa50c786c8f3f9715cf
Carlos Hernandez <139130873+car1810s@users.noreply.github.com>
2024-01-14 12:00:57 -0300

    Update CartWidget.js

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,14 +1,2 @@
 
 
-import cart from './assets/cart.svg'
-
-
-const CartWidget = () => {
-     return(
-        <div> 
-            <img src={cart} alt="cart-wdiget"/>
-        </div>
-     )
-}
-
-export default CartWidget
